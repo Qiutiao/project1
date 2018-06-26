2e980b0eccf5ffd99caa11650381cdf716407583
wzj421301214 <wzj421301214@163.com>
2018-06-26 14:11:51 +0800

    Initial Commit

---
+++ project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project1
+//
+//  Created by 王子健 on 2018/6/26.
+//  Copyright © 2018年 王子健. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
