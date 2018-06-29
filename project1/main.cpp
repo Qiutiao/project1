b5908e626aeca33f8314dd21a90d45a0a61f6a8a
wzj421301214 <wzj421301214@163.com>
2018-06-29 20:52:13 +0800

    test
    test1

--- project1/main.cpp
+++ project1/main.cpp
@@ -13,6 +13,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -20,16 +21,40 @@ const int MAX_NUM=1000;
 
 string Trim(string& str)
 {
+    //str.find_first_not_of(" \t\r\n"),在字符串str中从索引0开始，返回首次不匹配"\t\r\n"的位置
     str.erase(0,str.find_first_not_of(" \t\r\n"));
     str.erase(str.find_last_not_of(" \t\r\n") + 1);
     return str;
 }
 
 int a[MAX_NUM];
-class Poi{
-    int a;
+
+class csvdata
+{
+    public:
+        int id;
+        int level;
+        float price;
+        float cost;
 };
-int main(){
-    freopen("studentdate.number","r",stdin);
+
+int main(int argc,const char * argv[])
+{
+    vector<csvdata> incsv;
+    csvdata intp;
+    FILE *fp;
+    fp=fopen("/Users/S20171105125/Desktop/project1","r");//你自己的文件路径
+    while(1)
+        {
+            fscanf(fp,"%d,%d,%f,%f",&intp.id,&intp.level,&intp.price,&intp.cost);
+            incsv.push_back(intp);
+            if (feof(fp))break;
+        }
+    fclose(fp);
+    or(int i=0;i<incsv.size();i++)
+        {
+            cout<<incsv[i].id<<" "<<incsv[i].level<<" "<<incsv[i].price<<" "<<incsv[i].cost<<endl;
+            }//输出显示每行的数据
+    system("pause");
     return 0;
 }
