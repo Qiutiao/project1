fad1d827e08d1f32583dda146ec125f65d6f4949
wzj421301214 <40412864+wzj421301214@users.noreply.github.com>
2018-07-10 00:19:39 +0800

    完善程序
    1

--- project1/main.cpp
+++ project1/main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 //const int MAX_NUM=1000;
 //int a[Max_num];
 
-string Trim(string& str)
+string Delect(string& str)
 {
     //str.find_first_not_of(" \t\r\n"),在字符串str中从索引0开始，返回首次不匹配"\t\r\n"的位置
     str.erase(0,str.find_first_not_of(" \t\r\n"));
@@ -31,40 +31,48 @@ string Trim(string& str)
 class Students
 {
 	public:
-		char studentID[15];
-    	char name[20];
-    	char sex[10];
-    	char birth[30];
-    	char grade[20];
-    	char tel[20];
+		string studentID;
+    	string name;
+    	string sex;
+    	string birth;
+    	string grade;
+    	string tel;
+    	Students(string a=NULL,string b=NULL,string c=NULL,string d=NULL,string e=NULL,string f=NULL):
+			studentID(a),name(b),sex(c),birth(d),grade(e),tel(f){}
 };
 class Judges
 {
 	public:
-		char judge1[10];
-    	char judge2[10];
-    	char judge3[10];
-    	char judge4[10];
-    	char judge5[10];
+		string judge[5];
+		Judges(string j=NULL)
+		{
+			judge={j};score=s;
+		}
+		/*
+		string judge2[200];
+    	string judge3[200];
+    	string judge4[200];
+    	string judge5[200];
+    	*/
 };
 
-/*class Judges
+class Average
 {
-	private:
-    	int max,min,average;
 	public:
-		int judge[200];
-    	void information(int judge_temp[])
+		double max,min;
+		double average;
+		double judge[5];
+    	void information(string judge_temp[])
 		{
-        judge[0]=judge_temp[0];
-		judge[1]=judge_temp[1];
-		judge[2]=judge_temp[2];
-		judge[3]=judge_temp[3];
-		judge[4]=judge_temp[4];
+        	judge[0]=atof(judge_temp[0]);
+			judge[1]=atof(judge_temp[1]);
+			judge[2]=atof(judge_temp[2]);
+			judge[3]=atof(judge_temp[3]);
+			judge[4]=atof(judge_temp[4]);
     	}
     	void extremum(){
         	int i;
-        	int max=0,min=4;
+        	int max=0,min=0;
         	for(i=0;i<5;++i)
 			{
             	if(judge[max]<judge[i])
@@ -73,27 +81,30 @@ class Judges
 					{min=i;}
         	}
         average=(judge[0]+judge[1]+judge[2]+judge[3]+judge[4]-judge[max]-judge[min])/3;
-    }
-};*/
+    	}
+};
 
 int main(int argc,const char * argv[])
 {
     Students s[200];
     Judges j[200];
+    Average a[200];
     int i=0;
     int n=0;
     FILE *a;
     FILE *b;
     ifstream fin("/Users/20171105125/Desktop/project1/studentdata.csv");
     string line;
+	while (getline(fin, line))
+    {n++;}
 	if ((a=fopen("/Users/20171105125/Desktop/project1/studentdata.csv","r"))==1)
     {
-        while(fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5))
+        while(fscanf(a,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge[0],j[i].judge[1],j[i].judge[2],j[i].judge[3],j[i].judge[4]))
         {
             while(i>=n)
                 {return 0;}
-            printf("%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s\n",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5);
-            fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s\n",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5);
+            a[i].information(j[i].judge);
+            a[i]. extremum();
             i++;
         }
     }
@@ -102,11 +113,11 @@ int main(int argc,const char * argv[])
         printf("Open object file failure.\n");
     }
     b=fopen("/users/20171105125/Desktop/project1/studentdataout.csv","w");
-    
-    while (getline(fin, line))
-    {
-        n++;
-    }
-    
+    fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s,%s\n",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5,"average");
+    for(i=1;i<n;++n)
+	{
+		printf("%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s,%lf\n",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5,a[i].average);
+    	fprintf(b,"%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s,%lf\n",s[i].studentID,s[i].name,s[i].sex,s[i].birth,s[i].grade,s[i].tel,j[i].judge1,j[i].judge2,j[i].judge3,j[i].judge4,j[i].judge5,a[i].average);
+	}
     return 0;
 }
